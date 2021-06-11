3e93c97dfd5fa8b3b6496cedc7ead87d6c1e06ab
kimkim480 <lordkim480@gmail.com>
2021-06-11 09:36:13 -0300

    refactoring the application

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 export const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSpecificationController.handle);
 
 specificationsRoutes.get("/", (request, response) => {
