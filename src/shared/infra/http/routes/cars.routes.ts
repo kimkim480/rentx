16c3c72e98efc60dd9214ef8113e995257486061
kimkim480 <lordkim480@gmail.com>
2021-06-30 10:44:04 -0300

    add upload files to aws s3

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -17,7 +17,7 @@ const listAvailablesCarsController = new ListAvailablesCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
 const uploadCarImagesController = new UploadCarImagesController();
 
-const upload = multer(uploadConfig.upload("./tmp/cars"));
+const upload = multer(uploadConfig);
 
 carsRoutes.post(
   "/",
