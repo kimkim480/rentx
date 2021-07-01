bc9e6eb426711be7ca2cd282c164d9e377abd266
kimkim480 <lordkim480@gmail.com>
2021-07-01 09:22:30 -0300

    add babel configuration

---
+++ babel.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current"
+        }
+      }
+    ],
+    "@babel/preset-typescript"
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@modules": ["./src/modules"],
+          "@config": ["./src/config"],
+          "@shared": ["./src/shared"],
+          "@errors": ["./src/shared/errors"],
+          "@utils": ["./src/utils"],
+          "@middlewares": ["./src/shared/infra/http/middlewares"]
+        },
+      },
+    ],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
+};
